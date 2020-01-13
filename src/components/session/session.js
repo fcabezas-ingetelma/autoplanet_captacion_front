cebb971885e54440446febd588ee8a646cbd7664
Cristian Bruna <cristian.bruna@ingetelma.cl>
2020-01-13 15:55:40 -0300

    Vertical alignment de Ejecutivo Comercial y Rut

--- src/components/session/session.js
+++ src/components/session/session.js
@@ -9,18 +9,19 @@ class SessionHeader extends React.Component {
 
     render() {
         return (
-            <Container nameClass='align-middle'>
-                <Row >
-                    <Col>
-                    <div align='left' nameClass='align-middle'>
+            <Container >
+                <Row>
+                    <Col className="my-auto" >
+                    <div align='left' >
                         <label id="attendedText" >{(this.props.attenderRut !== '' && this.props.attenderRut !== undefined) ? 'Ejecutivo Comercial: ' + this.props.attenderRut : ''}</label>
                     </div>
                     </Col>
                     {this.props.rut && 
-                        <Col>
-                        <div align='right'>
-                            <label id="userText" >{(this.props.rut !== '' && this.props.rut !== undefined) ? 'Cliente: ' + this.props.rut : ''  } <Button variant='danger'>Salir</Button></label>
-                        </div>
+                        <Col className="my-auto" >
+                            <div align='right' >
+                                <label id="userText" >{(this.props.rut !== '' && this.props.rut !== undefined) ? 'Cliente: ' + this.props.rut : ''  }</label>
+                                <Button variant='danger'>Salir</Button>
+                            </div>
                         </Col>
                     }
                 </Row>
