00878abe75935e51167de333969329d3f3b7c084
Cristian Bruna <cristian.bruna@ingetelma.cl>
2020-01-13 12:39:58 -0300

    Eliminacion de puntos en rut

--- src/components/basic_data/input_data.js
+++ src/components/basic_data/input_data.js
@@ -48,16 +48,16 @@ class InputData extends React.Component {
             let value = e.target.value.replace(/\./g, '').replace('-', '');
   
             if (value.match(/^(\d{2})(\d{3}){2}(\w{1})$/)) {
-              value = value.replace(/^(\d{2})(\d{3})(\d{3})(\w{1})$/, '$1.$2.$3-$4');
+              value = value.replace(/^(\d{2})(\d{3})(\d{3})(\w{1})$/, '$1$2$3-$4');
             }
             else if (value.match(/^(\d)(\d{3}){2}(\w{0,1})$/)) {
-              value = value.replace(/^(\d)(\d{3})(\d{3})(\w{0,1})$/, '$1.$2.$3-$4');
+              value = value.replace(/^(\d)(\d{3})(\d{3})(\w{0,1})$/, '$1$2$3-$4');
             }
             else if (value.match(/^(\d)(\d{3})(\d{0,2})$/)) {
-              value = value.replace(/^(\d)(\d{3})(\d{0,2})$/, '$1.$2.$3');
+              value = value.replace(/^(\d)(\d{3})(\d{0,2})$/, '$1$2$3');
             }
             else if (value.match(/^(\d)(\d{0,2})$/)) {
-              value = value.replace(/^(\d)(\d{0,2})$/, '$1.$2');
+              value = value.replace(/^(\d)(\d{0,2})$/, '$1$2');
             }
             e.target.value = value;
         }
