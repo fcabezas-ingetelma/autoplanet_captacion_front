0b7cf1a45220bc26aafba307167058660dcfee09
Francisco Cabezas <fcabezas.berrios@gmail.com>
2020-01-13 17:39:02 -0300

    Add identation

--- src/components/basic_data/input_data.js
+++ src/components/basic_data/input_data.js
@@ -81,7 +81,7 @@ class InputData extends React.Component {
                             errors.rut = 'Campo Requerido';
                         } else if (!validaRut(values.rut)) {
                             errors.rut = 'Rut Inválido';
-                            }
+                        }
                         return errors;
                     }}
                     onSubmit = {(values, { setSubmitting }) => {
@@ -138,9 +138,9 @@ class InputData extends React.Component {
                         <Form onSubmit={handleSubmit}>
                             <Form.Group as={Row} controlId='rut'>
                                 <Col align='left'>
-                                <Form.Label sm={2}  >
-                                    RUT
-                                </Form.Label>
+                                    <Form.Label sm={2}  >
+                                        RUT
+                                    </Form.Label>
                                 </Col>
                                 <Col sm={10}>
                                     <Form.Control 
@@ -162,12 +162,12 @@ class InputData extends React.Component {
 
                             <Form.Group as={Row} controlId='cellphone'>
                                 <Col align='left'>
-                                <Form.Label sm={2} >
-                                    Teléfono Celular
-                                </Form.Label>
+                                    <Form.Label sm={2} >
+                                        Teléfono Celular
+                                    </Form.Label>
                                 </Col>
                                 <Col sm={10}>
-                                <InputGroup>
+                                    <InputGroup>
                                         <InputGroup.Prepend id="inputGroupPrepend">
                                             <InputGroup.Text>+569</InputGroup.Text>
                                         </InputGroup.Prepend>
@@ -182,17 +182,15 @@ class InputData extends React.Component {
                                             onChange={handleChange}
                                             placeholder="Ingrese los últimos 8 digitos" 
                                         />
-                                </InputGroup>
-                                    </Col>
-                                
+                                    </InputGroup>
+                                </Col>
                             </Form.Group>
 
                             <Form.Group as={Row} controlId='email'>
                                 <Col align='left'>
-                                <Form.Label >
-                                    Email  <label className="text-muted">(Opcional)</label>
-                                </Form.Label>
-                                
+                                    <Form.Label >
+                                        Email  <label className="text-muted">(Opcional)</label>
+                                    </Form.Label>
                                 </Col>
                                 <Col sm={10}>
                                     <Form.Control 
@@ -204,71 +202,67 @@ class InputData extends React.Component {
                                     />
                                 </Col>
                             </Form.Group>
-
                             
                             <Form.Group as={Row} controlId='clientType' value={values.clientType}>
                                 <Col align='left'>
-                                <Form.Label >
-                                    Tipo de Cliente
-                                </Form.Label>
+                                    <Form.Label >
+                                        Tipo de Cliente
+                                    </Form.Label>
                                 </Col>
                                 <Col align='left' sm={10}>
-                                {['Cabify','Cornershop','Empleado','Taller','Otro'].map(type =>(
-                                    <div key={`${type}`}>
-                                        <Form.Check
-                                            required
-                                            type='radio'
-                                            id={`${type}`}
-                                            label={`${type}`}
-                                            value={`${type}`}
-                                            onChange={handleChange}
-                                            name="clientType"
-                                        />
+                                    {['Cabify','Cornershop','Empleado','Taller','Otro'].map(type =>(
+                                        <div key={`${type}`}>
+                                            <Form.Check
+                                                required
+                                                type='radio'
+                                                id={`${type}`}
+                                                label={`${type}`}
+                                                value={`${type}`}
+                                                onChange={handleChange}
+                                                name="clientType"
+                                            />
                                         </div>
-                                        ))}
+                                    ))}
                                 </Col>
                             </Form.Group>
 
-                                <Form.Group as={Row} controlId='confirmationChoice' value={values.confirmationChoice}>
-                                    <Form.Label column sm={2}>
-                                        ¿Desea participar en campañas promocionales?
-                                    </Form.Label>
-                                    <Col>
-                                        <Form.Check
-                                            inline
-                                            required
-                                            type='radio'
-                                            id='si'
-                                            label='Sí'
-                                            name='confirmationChoice'
-                                            value='No'
-                                            onChange={handleChange}
-                                        />
-                                    </Col>
-                                    <Col>
-                                        <Form.Check
-                                            inline
-                                            required
-                                            type='radio'
-                                            id='no'
-                                            label='No'
-                                            name='confirmationChoice'
-                                            value='Si'
-                                            onChange={handleChange}
-                                        />
-                                    </Col>
-                                    
-                                               
-                                </Form.Group>
+                            <Form.Group as={Row} controlId='confirmationChoice' value={values.confirmationChoice}>
+                                <Form.Label column sm={2}>
+                                    ¿Desea participar en campañas promocionales?
+                                </Form.Label>
+                                <Col>
+                                    <Form.Check
+                                        inline
+                                        required
+                                        type='radio'
+                                        id='si'
+                                        label='Sí'
+                                        name='confirmationChoice'
+                                        value='No'
+                                        onChange={handleChange}
+                                    />
+                                </Col>
+                                <Col>
+                                    <Form.Check
+                                        inline
+                                        required
+                                        type='radio'
+                                        id='no'
+                                        label='No'
+                                        name='confirmationChoice'
+                                        value='Si'
+                                        onChange={handleChange}
+                                    />
+                                </Col> 
+                            </Form.Group>
                                 
+                            <input type="hidden" value="" name="attender_rut" id="hiddenRut" />
+                            <input type="hidden" value="" name="codeToValidate" id="hiddenCode" />
+                            <input type="hidden" value="" name="expires_at" id="hiddenExpiration" />
                                     
-                                        <input type="hidden" value="" name="attender_rut" id="hiddenRut" />
-                                        <input type="hidden" value="" name="codeToValidate" id="hiddenCode" />
-                                        <input type="hidden" value="" name="expires_at" id="hiddenExpiration" />
-                                    
-                                    <Button block type="submit" disabled={isSubmitting} variant="danger">
-                                        Continuar
-                                    </Button>
+                            <Button block type="submit" disabled={isSubmitting} variant="danger">
+                                Continuar
+                            </Button>
                         </Form>
                     </Container>
                 )}
