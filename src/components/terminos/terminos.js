8f2e4e8e3ea2fd35bb8b77c861ca712cf52005ef
Francisco Cabezas <fcabezas.berrios@gmail.com>
2020-02-01 01:07:53 -0300

    Add terminos text

--- src/components/terminos/terminos.js
+++ src/components/terminos/terminos.js
@@ -6,9 +6,91 @@ function TerminosAndCond() {
         <Container >
             <Row>
                 <h1>
-                    Términos y Condiciones
+                    Política de Privacidad y Protección de Datos
                 </h1>
-            </Row>             
+            </Row>   
+            <Row>
+                <h5>
+                    <p>
+                        Sociedad Comercializadora de Repuestos S.A. (“Autoplanet”), se compromete a garantizar que la información personal de sus clientes y usuarios se encuentre protegida, que no se utilice en forma indebida y asume la responsabilidad de cumplir con la legislación chilena vigente en materia de protección de datos, comprometiéndose a tratar los datos de manera lícita, leal y transparente.
+                        La presente política de privacidad y protección de datos detalla quién es el responsable, la finalidad, la legitimación del tratamiento de datos personales; las fuentes conforme a las cuales se recolectan datos personales, el uso de ellos y los derechos que le asisten al titular de dichos datos.
+                        Autoplanet entiende que Ud. al aceptar expresamente la presente política de privacidad, al entregar sus datos personales y dar uso de los servicios proveídos por Autoplanet, ha leído y entendido los términos del tratamiento de sus datos personales que realiza Autoplanet.
+                    </p>
+
+                    <p>
+                        Datos de menores
+                        Los servicios y aplicaciones de Autoplanet, no están dirigidos a menores de edad. Por tanto, se les instruye no registrarse como usuario de los sitios web, aplicaciones o servicios de Autoplanet. Si Autoplanet advirtiera que determinada información corresponde a un menor de edad, se procederá, en la medida de lo posible, a la eliminación de la misma.
+                    </p>
+
+                    <p>
+                        Responsable del tratamiento de datos
+                    </p>
+
+                    <p>
+                        Sociedad Comercializadora de Repuestos S.A.
+                    </p>
+                    <p>
+                        RUT: 76.762.740-8
+                    </p>
+                    <p>
+                        Domicilio: Av. Vicuña Mackenna N° 10.085, La Florida.
+                    </p>
+                    <p>
+                        Teléfono: +56 9 5372 2989
+                    </p>
+                    <p>
+                        Casilla de correo electrónico de contacto: catalinaorozco@autoplanet.cl
+                    </p>
+
+                    <p>
+                        Fundamento del tratamiento de datos:
+                        Autoplanet funda el tratamiento de datos personales en el cumplimiento de un contrato, obligaciones legales y/o en el consentimiento expreso, previo, inequívoco, libre e informado del titular. A mayor abundamiento, la información personal que el cliente y/o usuario suministra y aquella que legítimamente captura Autoplanet, sólo podrá ser utilizada de acuerdo a esta política de privacidad, salvo excepciones dispuestas por la ley. 
+                    </p>
+
+                    <p>
+                        Naturaleza de los datos objeto del tratamiento:
+                        De acuerdo a las finalidades que se describen en el presente documento, Autoplanet trata datos personales de sus clientes y usuarios, tales como de identificación (nombre, apellido, rut, domicilio, número telefónico y correo electrónico), de medios de pagos (número de tarjetas de crédito/debito, banco y número de cuenta corriente); historial crediticio, demográficos, relacionados con sus preferencias y gustos, datos de identificación de vehículos, tales como: placa patente, VIN, número de motor, marca, modelo y año y otros pudiendo incluso tratarse de datos sensibles.
+                    </p>
+
+                    <p>
+                        Canales de captación:
+                        Autoplanet obtiene los datos personales de sus clientes y usuarios a través de diferentes medios, por ejemplo, cuando el propio titular entrega dicha información a través de algún sitio web, aplicación de Autoplanet, correo electrónico, teléfono móvil, al completar un formulario en papel o web, al contratar Servicios, al comprar o cotizar bienes y/o servicios ofrecidos por Autoplanet o por sus empresas relacionadas, cuando específica y expresamente es autorizado por el titular de los datos. En cuyos casos Autoplanet siempre informará del responsable del tratamiento, la finalidad del mismo, los destinatarios de la información, así como la forma de ejercer los derechos que le otorga la legislación vigente en protección de datos. 
+                        Asimismo, Autoplanet trata información de sus clientes y usuarios en forma anónima, para efectos estadísticos, tales como visitas a los sitios web y visitas a puntos de venta de Autoplanet o de sus empresas relacionadas. Tratándose de la captación de datos personales en redes sociales, es importante recalcar que aquellas tienen sus propias políticas de privacidad, por lo que se recomienda revisar dichas políticas con el objeto de conocer como éstos recopilan y comparten los datos personales de sus seguidores. Finalmente, Autoplanet también podrá captar datos personales de sus clientes y usuarios de fuentes de libre acceso público.
+                    </p>
+
+                    <p>
+                        Finalidad del tratamiento:
+                        El tratamiento que realiza Autoplanet de los datos personales de los clientes y/o usuarios, tendrá por objeto: i) la realización de todas aquellas actividades y operaciones tendientes a proveer los bienes o servicio contratado; ii) para el mejoramiento del servicio; iii) suministrar información comercial, legal, de productos y servicios, de seguridad o de cualquier otra índole dentro de los bienes o servicios; iv) conocer datos de contacto del cliente y/o ususario para efectos de proveer el servicio; v) envío de notificaciones con fines de seguridad, ofrecimiento de beneficios y ofertas comerciales, de información de productos y servicios de empresas relacionadas con Autoplanet y la industria automotriz y de publicidad; vi) comunicaciones necesarias para mantener comunicado e informado al cliente mediante: llamada telefónica, mensaje de texto (SMS), correo electrónico, notificaciones emanadas de aplicaciones móviles, redes sociales tales como: Facebook, Twitter, Instagram o cualquier red social de integración, entre otros; vii) comunicaciones de cobranza mediante: llamadas telefónicas, mensaje de texto (SMS), correo electrónico, notificaciones emanadas de aplicaciones móviles; viii) realizar encuestas de satisfacción concerniente a los bienes y servicios prestados; ix) ofrecer nuevos bienes o servicios que mejoren la experiencia de los clientes y/o usuario o beneficios adicionales a los convenidos, x) gestiones de cobranza ya sea directamente por Autoplanet o mediante un tercero contratado para tal función; xi) prevenir fraudes; xii) fines legales, xiii) realizar perfiles y análisis sobre el comportamiento de los clientes y/o usuarios respecto de los bienes y servicios, la página web, micrositios y/o aplicaciones; xiv) para mantener un registro de las comunicaciones orales, escritas, telefónicas, electrónicas o por cualquier otro medio que mantenga con el cliente; xv) fines estadísticos o demográficos en forma agregada.
+                    </p>
+
+                    <p>
+                        Cesión de datos:
+                        Autoplanet mantendrá estricta reserva respecto de los datos personales que trata. Sin embargo, existe la posibilidad de que resulte necesario compartir tales datos personales con terceros, situados en Chile o en el extranjero, tales como empresas relacionadas, proveedores, organismos públicos, tribunales de justicia, entidades fiscales, y, en general, con entidades necesarias para la prestación de los bienes o servicios proveídos.
+                        Con el objeto de complementar los bienes o servicios proveídos y/o ofrecidos por Autoplanet, esta podrá compartir sus datos con las empresas relacionados con Autoplanet de acuerdo a la ley 18.045, debiendo el encargado del tratamiento de datos dar cumplimiento a la presente política de privacidad.
+                    </p>
+
+                    <p>
+                        Enlaces a sitios web de terceros:
+                        En el caso que Autoplanet provea o disponga de enlaces a sitios web operados y/o controlados por terceros ajenos a Autoplanet, éste no dispone de control alguno respecto de dichos sitios por tanto, no es responsable del contenido de los mismos y no tiene control del tratamiento de datos que ellos realizan, debiendo en tal caso, someterse a las políticas de privacidad dispuestas por estos terceros, por lo que Autoplanet recomienda revisar detalladamente las políticas de privacidad antes de utilizar estos sitios web, para cerciorarse que se encuentra conforme con la forma en la que sus datos personales se recopilan y comparten.
+                    </p>
+
+                    <p>
+                        Seguridad de la información:
+                        Autoplanet se compromete a tratar toda la información de sus titulares con reserva y a adoptar mediante las medidas técnicas y organizativas adecuadas y a los resguardos posibles y razonables de conformidad con los estándares y prácticas de la industria relacionados con la seguridad de la información, limitando en lo posible y razonable el acceso de terceros y velará para que estos terceros sólo accedan a los datos personales necesarios para el cumplimiento del objeto y la no utilización de ellos para un fin distinto a aquel requerido y autorizado por Autoplanet. A mayor abundamiento, Autoplanet solicita a estos terceros que apliquen el mismo grado de protección de los datos que este aplica.
+                    </p>
+
+                    <p>
+                        Derechos del titular de datos personales:
+                        Todo titular de datos podrá, en cualquier momento ejercer sus derechos de información, actualización, eliminación y cancelación y/o bloqueo de sus datos personales, conforme con lo establecido en la normativa vigente, contactando a Catalina Orozco, mail, Catalinaorozco@autoplanet.cl. El ejercicio de los derechos señalados precedentemente tendrá como límite las excepciones previstas por la Ley y la prestación de las obligaciones por adquisición de bienes y servicios.
+                        Conforme a los procedimientos de control y seguridad de la información de Autoplanet, se le solicitará al titular, prueba de identidad antes de compartir los datos personales y ejercer los derechos antes señalados.
+                    </p>
+
+                    <p>
+                        Vigencia del tratamiento de datos:
+                        Autoplanet sólo almacenará los datos personales del cliente y/o ususario en la medida que sea necesario para el cumplimiento de las finalidades antes indicadas, mientras exista una relación contractual y/o comercial con el titular de los datos, y no haya ejercido el derecho de cancelación del tratamiento de datos.
+                    </p>
+                </h5>
+            </Row>          
         </Container>
     );
 }
